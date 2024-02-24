f6b8fa0f657124a375a1aebe5dbd20c4a1a2d774
pouria <puriyaj@gmail.com>
2024-02-24 11:00:09 +0100

    del

--- app/api/banners/route.ts
+++
@@ -1,7 +0,0 @@
-import { prisma } from "@utils/prisma";
-import { NextRequest, NextResponse } from "next/server";
-
-export async function GET(req: NextRequest) {
-  const result = await prisma.banner.findMany();
-  return NextResponse.json(result);
-}

aad65276ed705aef7d1f7cc0f18d363a4a5c34aa
pouria <puriyaj@gmail.com>
2024-02-26 23:48:09 +0100

    I

--- app/(dashboard)/dashboard/banners/banners-list.tsx
+++ app/(dashboard)/dashboard/banners/banners-list.tsx
@@ -3,7 +3,7 @@ import { Button, Table, TableHead } from '@cmp/ui';
 import React, { useMemo } from 'react';
 import Image from 'next/image';
 import { ActionButton } from '@cmp/action-button';
-import { Banner } from '../../../../prisma/generated/client';
+import { BANNER_POSITION, Banner } from '../../../../prisma/generated/client';
 import moment from 'moment-jalaali';
 import { handleError } from '@utils/err-handler';
 import { toast } from 'react-toastify';
@@ -12,6 +12,7 @@ import { useBannerAddModal } from './banner-add-modal';
 import { DELETE_MSG, activation_status_details, banner_position_details } from '@utils/constants';
 import { Badge } from '@cmp/ui/components/badge';
 import { useConfirmation } from '@utils/hooks/confirmation.provider';
+import { number } from 'yup';
 
 type Props = {
   banners: Banner[];
@@ -22,7 +23,7 @@ export const BannersList: React.FC<Props> = ({ banners }) => {
   const { deleteConfirmation } = useConfirmation();
   const list = useMemo(
     () =>
-      banners?.map((item, idx) => ({
+      banners?.map((item: Banner, idx) => ({
         id: item.id,
         rowsNum: idx + 1,
         title: item.title,

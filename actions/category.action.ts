7dbd675c7df46077d82ce4903cb462122ed928f2
pouria <puriyaj@gmail.com>
2024-02-27 11:06:31 +0100

    i

--- actions/category.action.ts
+++ actions/category.action.ts
@@ -24,7 +24,7 @@ export const getCategories = async (page: number = 1, query?: string): Promise<P
   return result;
 };
 export const allCategories = async (): Promise<CategoryWithChildrens[]> => {
-  const url = `categories/all`;
+  const url = `api/categories/all`;
   const response = await fetch(url, { headers: { 'Content-Type': 'application/json', Accept: 'application/json' }, next: { tags: [CACHE_CATEGORIES] } });
 
   const result = await response.json();

ca9e29e95ff9515c79f147866c218347ca52bbd0
pouria <puriyaj@gmail.com>
2024-02-27 09:04:12 +0100

    i

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,7 @@
 import { ACTIVATION_STATUS, BANNER_POSITION, CURRENCY_TYPE } from '../prisma/generated/client'
 
-export const CLIENT_IP = process.env.NODE_ENV == 'production' ? 'https://kaufmann-web-f4wd1to2c-puriyajs-projects.vercel.app/' : 'http://localhost:3000';
-export const API_IP = process.env.NODE_ENV == 'production' ? 'https://api.kaufmann-web-f4wd1to2c-puriyajs-projects.vercel.app/' : 'http://localhost:3000/api';
+export const CLIENT_IP = process.env.NODE_ENV == 'production' ? 'https://kaufmann-web-app.vercel.app/' : 'http://localhost:3000';
+export const API_IP = process.env.NODE_ENV == 'production' ? 'https://kaufmann-web-app.vercel.app/api' : 'http://localhost:3000/api';
 export const ADMIN_URL = `${CLIENT_IP}`;
 export const API_URL = process.env.NEXTAUTH_URL;
 // export const API_URL = `${API_IP}`;

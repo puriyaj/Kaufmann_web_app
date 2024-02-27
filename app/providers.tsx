bf0513c27a879f0859ec792b9fe15f995740d22a
pouria <puriyaj@gmail.com>
2024-02-27 01:00:25 +0100

    i

--- app/providers.tsx
+++ app/providers.tsx
@@ -10,7 +10,7 @@ export function Providers({ children }: any) {
 
   return (
     <>
-      <SessionProvider>
+      <SessionProvider basePath="/api/auth">
         <ConfirmationProvider>
           <ActionContextProvider>{children}</ActionContextProvider>
         </ConfirmationProvider>
